1ff06b2d8efe1640bc71f8cbe5c96754f77684d6
ATTAQA <rohitmulawkar@gmail.com>
2025-01-12 07:42:53 +0530

    df

---
+++ Ex1.java
@@ -0,0 +1,9 @@
+public class Ex1 {
+
+    public static void main(String args[]) {
+        String str = "rohit@gmail.com";
+        String words[] = str.split("@")
+        for(int i; i<=str.lenth()-1;i++) {
+        System.out.println(words[i])   ;}
+
+    }}
